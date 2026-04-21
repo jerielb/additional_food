0181fc28340c0f627e352f68dae8dffbbb173dc1
jerielB <jerielbongco98@gmail.com>
2026-04-21 18:19:23 +1000

    working villager professions

--- src/main/java/com/jerielb/additional_food/AdditionalFood.java
+++ src/main/java/com/jerielb/additional_food/AdditionalFood.java
@@ -5,11 +5,9 @@ import com.jerielb.additional_food.block.ModBlocks;
 //import com.jerielb.additional_food.entity.custom.JollibeeEntity;
 import com.jerielb.additional_food.item.ModItemGroups;
 import com.jerielb.additional_food.item.ModItems;
-//import com.jerielb.additional_food.villager.ModVillagers;
+import com.jerielb.additional_food.villager.ModVillagers;
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
-import net.minecraft.world.item.ItemStack;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,65 +21,7 @@ public class AdditionalFood implements ModInitializer {
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 
-//		ModVillagers.registerVillagers();
-//		ModVillagers.registerVillagers();
-//		// Jollibee Employee Trades
-//		// NOVICE
-//		TradeOfferHelper.registerVillagerOffers(ModVillagers.JOLLIBEE_EMP, 1, factories -> {
-//			factories.add(((entity, random) -> new TradeOffer(
-//					new TradedItem(Items.EMERALD, 4),
-//					new ItemStack(ModItems.FRIES, 1), 16, 2, 0.04f)
-//			));
-//			factories.add(((entity, random) -> new TradeOffer(
-//					new TradedItem(Items.EMERALD, 4),
-//					new ItemStack(ModItems.CHICKEN_NUGGETS, 1), 16, 2, 0.04f)
-//			));
-//		});
-//		// APPRENTICE
-//		TradeOfferHelper.registerVillagerOffers(ModVillagers.JOLLIBEE_EMP, 2, factories -> {
-//			factories.add(((entity, random) -> new TradeOffer(
-//					new TradedItem(Items.EMERALD, 8),
-//					new ItemStack(ModItems.YUM_BURGER, 1), 16, 4, 0.04f)
-//			));
-//			factories.add(((entity, random) -> new TradeOffer(
-//					new TradedItem(Items.EMERALD, 8),
-//					new ItemStack(ModItems.CHAMP_BURGER, 1), 16, 4, 0.04f)
-//			));
-//		});
-//		// JOURNEYMAN
-//		TradeOfferHelper.registerVillagerOffers(ModVillagers.JOLLIBEE_EMP, 3, factories -> {
-//			factories.add(((entity, random) -> new TradeOffer(
-//					new TradedItem(Items.EMERALD, 8),
-//					new ItemStack(ModItems.JOLLY_HOTDOG, 1), 16, 8, 0.04f)
-//			));
-//			factories.add(((entity, random) -> new TradeOffer(
-//					new TradedItem(Items.EMERALD, 12),
-//					new ItemStack(ModItems.JOLLY_SPAGHETTI, 1), 16, 8, 0.04f)
-//			));
-//		});
-//		// EXPERT
-//		TradeOfferHelper.registerVillagerOffers(ModVillagers.JOLLIBEE_EMP, 4, factories -> {
-//			factories.add(((entity, random) -> new TradeOffer(
-//					new TradedItem(Items.EMERALD, 12),
-//					new ItemStack(ModItems.BURGER_STEAK, 1), 16, 16, 0.04f)
-//			));
-//			factories.add(((entity, random) -> new TradeOffer(
-//					new TradedItem(Items.EMERALD, 16),
-//					new ItemStack(ModItems.PALABOK, 1), 16, 16, 0.04f)
-//			));
-//		});
-//		// MASTER
-//		TradeOfferHelper.registerVillagerOffers(ModVillagers.JOLLIBEE_EMP, 5, factories -> {
-//			factories.add(((entity, random) -> new TradeOffer(
-//					new TradedItem(Items.EMERALD, 16),
-//					new ItemStack(ModItems.CHICKEN_JOY, 1), 16, 32, 0.04f)
-//			));
-//			factories.add(((entity, random) -> new TradeOffer(
-//					new TradedItem(Items.EMERALD, 4),
-//					new ItemStack(ModItems.PEACH_MANGO_PIE, 1), 16, 32, 0.04f)
-//			));
-//		});
-//		
+		ModVillagers.registerVillagers();
 //		FabricDefaultAttributeRegistry.register(ModEntities.JOLLIBEE, JollibeeEntity.createAttributes());
 	}
 }
--- src/main/java/com/jerielb/additional_food/villager/ModVillagers.java
+++ src/main/java/com/jerielb/additional_food/villager/ModVillagers.java
@@ -3,45 +3,79 @@ package com.jerielb.additional_food.villager;
 import com.google.common.collect.ImmutableSet;
 import com.jerielb.additional_food.AdditionalFood;
 import com.jerielb.additional_food.block.ModBlocks;
-import net.fabricmc.fabric.api.object.builder.v1.world.poi.PointOfInterestHelper;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.Identifier;
-import net.minecraft.village.VillagerProfession;
-import net.minecraft.world.poi.PointOfInterestType;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import net.fabricmc.fabric.api.object.builder.v1.world.poi.PoiHelper;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.entity.ai.village.poi.PoiType;
+import net.minecraft.world.entity.npc.villager.VillagerProfession;
+import net.minecraft.world.item.trading.TradeSet;
+import net.minecraft.world.level.block.Block;
+
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class ModVillagers {
-	public static final RegistryKey<PointOfInterestType> JOLLIBEE_EMP_POI_KEY = registerPoiKey("jollibee_emp_poi");
-	public static final PointOfInterestType JOLLIBEE_EMP_POI = registerPOI("jollibee_emp_poi", ModBlocks.QUARTZ_BLOCK);
+	public static PoiType JOLLIBEE_EMP_POI;
+	public static VillagerProfession JOLLIBEE_EMP;
+
+//	public static final RegistryKey<PointOfInterestType> STREET_FOOD_VENDOR_POI_KEY = registerPoiKey("street_food_vendor_poi");
+//	public static final PointOfInterestType STREET_FOOD_VENDOR_EMP_POI = registerPOI("street_food_vendor_poi", Blocks.REINFORCED_DEEPSLATE);
+//	public static final VillagerProfession STREET_FOOD_VENDOR = registerProfession("street_food_vendor", STREET_FOOD_VENDOR_POI_KEY);
 	
-	public static final VillagerProfession JOLLIBEE_EMP = registerProfession("jollibee_emp", JOLLIBEE_EMP_POI_KEY);
-	
-	public static final RegistryKey<PointOfInterestType> STREET_FOOD_VENDOR_POI_KEY = registerPoiKey("street_food_vendor_poi");
-	public static final PointOfInterestType STREET_FOOD_VENDOR_EMP_POI = registerPOI("street_food_vendor_poi", Blocks.REINFORCED_DEEPSLATE);
-	
-	public static final VillagerProfession STREET_FOOD_VENDOR = registerProfession("street_food_vendor", STREET_FOOD_VENDOR_POI_KEY);
-	
-	private static VillagerProfession registerProfession(String name, RegistryKey<PointOfInterestType> type) {
-		return Registry.register(Registries.VILLAGER_PROFESSION, Identifier.of(AdditionalFood.MOD_ID, name),
-				new VillagerProfession(name, entry -> entry.matchesKey(type), entry -> entry.matchesKey(type),
-						ImmutableSet.of(), ImmutableSet.of(), SoundEvents.ENTITY_VILLAGER_WORK_LIBRARIAN));
+	private static VillagerProfession registerProfession(String name, String poiName, Supplier<SoundEvent> soundEvent) {
+		ResourceKey<PoiType> poiKey = ResourceKey.create(Registries.POINT_OF_INTEREST_TYPE, Identifier.fromNamespaceAndPath("additional_food", poiName));
+		Predicate<Holder<PoiType>> heldWorkstation = (entry) -> {
+			return entry.is(poiKey);
+		};
+		Predicate<Holder<PoiType>> acquirableWorkstation = (entry) -> {
+			return entry.is(poiKey);
+		};
+		ResourceKey<VillagerProfession> profKey = ResourceKey.create(Registries.VILLAGER_PROFESSION, Identifier.fromNamespaceAndPath("additional_food", name));
+		VillagerProfession profession = new VillagerProfession(Component.translatable("entity.additional_food.villager." + name), heldWorkstation, acquirableWorkstation, ImmutableSet.of(), ImmutableSet.of(), (SoundEvent)soundEvent.get(), buildTradeSets(name));
+		return (VillagerProfession) Registry.register(BuiltInRegistries.VILLAGER_PROFESSION, profKey, profession);
 	}
-	
-	private static PointOfInterestType registerPOI(String name, Block block) {
-		return PointOfInterestHelper.register(Identifier.of(AdditionalFood.MOD_ID, name),
-				1, 1, block);
+
+	private static PoiType registerPoi(String name, Block block) {
+		return PoiHelper.register(Identifier.fromNamespaceAndPath(AdditionalFood.MOD_ID, name), 1, 1, new Block[]{block});
 	}
-	
-	private static RegistryKey<PointOfInterestType> registerPoiKey(String name) {
-		return RegistryKey.of(RegistryKeys.POINT_OF_INTEREST_TYPE, Identifier.of(AdditionalFood.MOD_ID, name));
+
+	private static Int2ObjectMap<ResourceKey<TradeSet>> buildTradeSets(String professionName) {
+		Int2ObjectOpenHashMap<ResourceKey<TradeSet>> map = new Int2ObjectOpenHashMap();
+		
+		for(int level = 1; level <= 5; ++level) {
+			map.put(level, ResourceKey.create(Registries.TRADE_SET, Identifier.fromNamespaceAndPath("additional_food", professionName + "/level_" + level)));
+		}
+		
+		return map;
 	}
-	
+
 	public static void registerVillagers() {
 		AdditionalFood.LOGGER.info("Registering Villagers for " + AdditionalFood.MOD_ID);
+		
+		JOLLIBEE_EMP_POI = registerPoi("jollibee_emp_poi", ModBlocks.QUARTZ_BLOCK);
+		JOLLIBEE_EMP = registerProfession("jollibee_emp", "jollibee_emp_poi", () -> {
+			return (SoundEvent)BuiltInRegistries.SOUND_EVENT.getValue(Identifier.fromNamespaceAndPath("minecraft", "block.wool.hit"));
+		});
+				
+		BuiltInRegistries.POINT_OF_INTEREST_TYPE.entrySet().forEach((entry) -> {
+			if (((ResourceKey)entry.getKey()).identifier().getNamespace().equals("additional_food")) {
+				AdditionalFood.LOGGER.info("POI: {} -> {}", ((ResourceKey)entry.getKey()).identifier(), entry.getValue());
+			}
+			
+		});
+		BuiltInRegistries.VILLAGER_PROFESSION.entrySet().forEach((entry) -> {
+			if (((ResourceKey)entry.getKey()).identifier().getNamespace().equals("additional_food")) {
+				AdditionalFood.LOGGER.info("Profession: {} -> {}", ((ResourceKey)entry.getKey()).identifier(), entry.getValue());
+			}
+			
+		});
 	}
 }
